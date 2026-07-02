335259b767d581124dee4fb960147fb9f3efbdf2
xyx <xyx@qq.com>
2026-07-02 10:01:18 +0800

    修复Gateway: 显式声明WebClient.Builder Bean解决SpringBoot4.x兼容性

--- yunxingcloud/yunxingcloud-gateway/src/main/java/com/yunxingcloud/gateway/GatewayApplication.java
+++ yunxingcloud/yunxingcloud-gateway/src/main/java/com/yunxingcloud/gateway/GatewayApplication.java
@@ -2,10 +2,17 @@ package com.yunxingcloud.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @Bean
+    WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
     }
 }
